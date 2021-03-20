ac55a62cc1c1403c488c6cd42e9d62f35de15c7d
callie rogers <callie1013@gmail.com>
2021-03-20 01:32:51 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,17 +1,15 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
+row = "   |   |   "
+separator = "-----------"
 
 def display_board
   puts "A Tic Tac Toe Board"
 end
 
-row = "   |   |   "
-separator = "-----------"
-
-
-def display_board
-  puts "row"
-  puts "separator"
-  puts "row"
-  puts "separator"
-  puts "row"
+def "TTT board"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
 end
